436e3fc7ced5053efb77536ddf7ab4241b735061
joaorezends <joaocarlosrezendejunior@gmail.com>
2021-06-29 00:01:59 -0300

    add api documentation

--- app/Presentation/Api/routes/api.php
+++ app/Presentation/Api/routes/api.php
@@ -5,6 +5,17 @@ use App\Presentation\Api\Http\ShipordersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="PHP Challenge API",
+ *      @OA\Contact(
+ *          name="joaorezends",
+ *          url="https://github.com/joaorezends"
+ *      )
+ * )
+ */
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,6 +31,76 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * @OA\Get(
+ *      path="/api/people",
+ *      tags={"People"},
+ *      summary="Get list of people",
+ *      description="Returns list of people",
+ *      @OA\Response(response=200, description="Successful Operation"),
+ *      @OA\Response(response=400, description="Bad Request")
+ * )
+ *
+ * Returns list of people
+ */
+
+/**
+ * @OA\Get(
+ *      path="/api/people/{id}",
+ *      tags={"People"},
+ *      summary="Get a person",
+ *      description="Returns a person",
+ *      @OA\Parameter(
+ *          name="id",
+ *          description="Person id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(
+ *              type="integer"
+ *          )
+ *      ),
+ *      @OA\Response(response=200, description="Successful Operation"),
+ *      @OA\Response(response=400, description="Bad Request"),
+ *      @OA\Response(response=404, description="Resource Not Found")
+ * )
+ *
+ * Returns a person
+ */
 Route::apiResource("people", PeopleController::class)->only("index", "show");
 
+/**
+ * @OA\Get(
+ *      path="/api/shiporders",
+ *      tags={"Shiporder"},
+ *      summary="Get list of shiporders",
+ *      description="Returns list of shiporders",
+ *      @OA\Response(response=200, description="Successful Operation"),
+ *      @OA\Response(response=400, description="Bad Request")
+ * )
+ *
+ * Returns list of shiporders
+ */
+
+/**
+ * @OA\Get(
+ *      path="/api/shiporders/{id}",
+ *      tags={"Shiporder"},
+ *      summary="Get a shiporder",
+ *      description="Returns a shiporder",
+ *      @OA\Parameter(
+ *          name="id",
+ *          description="Shiporder id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(
+ *              type="integer"
+ *          )
+ *      ),
+ *      @OA\Response(response=200, description="Successful Operation"),
+ *      @OA\Response(response=400, description="Bad Request"),
+ *      @OA\Response(response=404, description="Resource Not Found")
+ * )
+ *
+ * Returns a shiporder
+ */
 Route::apiResource("shiporders", ShipordersController::class)->only("index", "show");
